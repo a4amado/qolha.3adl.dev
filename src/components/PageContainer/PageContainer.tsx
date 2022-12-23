2f50763eb5ffda9672e8b6481d3066fd4d4dfbc3
Ahmad Addel <a4addel@gmail.com>
2022-12-23 07:35:23 -0800

    create PageContainer

--- src/components/PageContainer/PageContainer.tsx
+++ src/components/PageContainer/PageContainer.tsx
@@ -1,3 +1,7 @@
 import { Row } from "antd";
 
-export default function PageContainer() {}
+export default function PageContainer(props: React.PropsWithChildren) {
+    return <Row className="block mx-auto w-full max-w-5xl">
+        {props.children}
+    </Row>
+}
