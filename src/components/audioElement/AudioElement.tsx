7bf0290a0dd301f49c4e443bd0221b14d55e8c38
Ahmad Addel <a4addel@gmail.com>
2023-01-01 05:25:20 -0800

    Refactor AudioElement

--- src/components/audioElement/AudioElement.tsx
+++ src/components/audioElement/AudioElement.tsx
@@ -1,11 +1,25 @@
-import { Popover, Col, Collapse } from "antd";
+import { Col, Collapse, Divider, List, Row, Typography } from "antd";
 import { memo } from "react";
-import { useToggle } from "react-use";
 
-const { Panel } = Collapse
-function AudioElement({ url, key}: { url: string, key: string }) {
-    
-    return  <p>{url}</p>
+const { Panel } = Collapse;
+
+function AudioElement({ url, key }: { url: string; key: string }) {
+  return (
+    <Row className="block border border-balck p-2 rounded max-w-xl w-full mx-auto">
+      <Typography.Title className="block">كلمة عربية </Typography.Title>
+      <Row className="block">
+        {Array.from({ length: 6 }).map((e, i) => {
+          return (
+            <Row className="flex flex-row items-stretch">
+              <Col>s</Col>
+              <Col className="flex-grow">s</Col>
+              <Col>s</Col>
+            </Row>
+          );
+        })}
+      </Row>
+    </Row>
+  );
 }
 
-export default memo(AudioElement)
+export default memo(AudioElement);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Col, Row, Menu, Collapse } from "antd";
+import { Col, Row, Menu, Collapse, Typography } from "antd";
 import React from "react";
 import Header from "../components/header";
 import PageContainer from "../components/PageContainer";
@@ -8,7 +8,6 @@ import * as yup from "yup";
 import { auth } from "../server/firebase";
 import { onAuthStateChanged } from "firebase/auth";
 import AudioElement from "../components/audioElement";
-
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   if (!ctx.query.q && !ctx.query.word) return { props: { home: true } };
@@ -42,7 +41,6 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 };
 
 export default function Page(props: any) {
-
   const isHome = !!props.home;
   const isError = !!props.error;
   const isWord = !!props.word;
@@ -53,50 +51,42 @@ export default function Page(props: any) {
         <Header isSearch={true} />
         <PageContainer>
           {isError && <TargetWord word="حدثَ خطاََ ما." />}
-          
-            {/* {isHome && (
-              <>
-                <Typography.Title className="!text-4xl !sm:text-5xl !md:text-6xl !lg:text-7xl block w-full">
-                  قُلها
-                </Typography.Title>
 
-                <Typography.Paragraph className="block w-full">
-                  قاموس صوتي للكلمات العربية
-                </Typography.Paragraph>
+          {isHome && (
+            <>
+              <Typography.Title className="!text-4xl !sm:text-5xl !md:text-6xl !lg:text-7xl block w-full">
+                قُلها
+              </Typography.Title>
 
+              <Typography.Paragraph className="block w-full">
+                قاموس صوتي للكلمات العربية
+              </Typography.Paragraph>
+
+              <Typography.Text className="block w-full">
+                كيف بدئنا ؟
+              </Typography.Text>
+
+              <Typography.Text className="block w-full">
+                لما رأيت معناة أصدقائي الاجانب في تعلم العربية خاصة عندما
+                يتعبق الامر بالنطق قررت أن ابني هذا الموقع لتسهيل الامر عليهم،
+              </Typography.Text>
+
+              <Typography.Text className="block w-full">
+                كيف تساعدنا ؟
                 <Typography.Text className="block w-full">
-                  كيف بدئنا ؟
+                  إذا أردت مساعدتنا عن طريق المساهمة في نطق الكلمات{" "}
+                  <Typography.Link href="/contribute">
+                    إضغط هُنا
+                  </Typography.Link>
                 </Typography.Text>
-
-                <Typography.Text className="block w-full">
-                  لما رأيت معناة أصدقائي الاجانب في تعلم العربية خاصة عندما
-                  يتعبق الامر بالنطق قررت أن ابني هذا الموقع لتسهيل الامر عليهم،
-                </Typography.Text>
-
-                <Typography.Text className="block w-full">
-                  كيف تساعدنا ؟
-                  <Typography.Text className="block w-full">
-                    إذا أردت مساعدتنا عن طريق المساهمة في نطق الكلمات{" "}
-                    <Typography.Link href="/contribute">
-                      إضغط هُنا
-                    </Typography.Link>
-                  </Typography.Text>
-                </Typography.Text>
-              </>
-            )} */}
-            {/* {isError && "Error"}
-            {isWord && "Word"} */}
+              </Typography.Text>
+            </>
+          )}
 
 
-            <Collapse bordered accordion className="w-full">
-              {Array.from({ length: 21 }).map((e, i) => {
-                return <Collapse.Panel header="كلمة عربية." key={i.toString()}>
-                <AudioElement key={i.toString()} url="كلمة عربية."/>  
-                </Collapse.Panel>
-                
-              })}
-            </Collapse>
-          
+          {
+            isWord && <AudioElement key="1" url="كلمة عربية." />
+          }
         </PageContainer>
       </Row>
     </>
