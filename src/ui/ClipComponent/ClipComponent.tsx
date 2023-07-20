3c1566fb5705f593794d5a9ef49bb2e2b585498e
Ahmad Addel <a4addel@gmail.com>
2023-07-20 21:45:21 +0300

    Update ClipComponent.tsx

--- src/ui/ClipComponent/ClipComponent.tsx
+++ src/ui/ClipComponent/ClipComponent.tsx
@@ -1,14 +1,16 @@
 import { ButtonGroup, Button } from "@blueprintjs/core";
 import { trpc } from "@utils/trpc";
+import { useSession } from "next-auth/react";
 import { useState } from "react";
 import { useAudio } from "react-use";
 
-export default function ClipComponent({ number, ar, username, clipId, ClipName }: {
-    number: number, ar: string, username: string, clipId: string, ClipName: string
+export default function ClipComponent({ number, ar, username, clipId }: {
+    number: number, ar: string, username: string, clipId: string
 }) {
     const rej = trpc.clip.reject.useMutation()
     const acc = trpc.clip.accept.useMutation()
     const disabled = useState(false);
+    const session = useSession();
     const audio = useAudio({
         src: `/api/clip/${clipId}/stream`
     })
@@ -18,17 +20,21 @@ export default function ClipComponent({ number, ar, username, clipId, ClipName }
         {audio[0]}
         <ButtonGroup className="gap-2">
             <Button disabled={disabled[0]} onClick={audio[2].play} small icon="play" text="play" />
-            <Button disabled={disabled[0]}
+            {/** @ts-ignore */}
+            {session.status === "authenticated" && ["owner", "admin"].includes(session.data?.user?.role || "") && <Button disabled={disabled[0]}
                 onClick={async () => {
                     await acc.mutateAsync({ clipId: clipId })
                     disabled[1](true)
 
                 }}
                 small intent="primary" text="accept" />
-            <Button disabled={disabled[0]} onClick={async () => {
+            }
+            {/** @ts-ignore */}
+            {session.status === "authenticated" && ["owner", "admin"].includes(session.data?.user?.role || "") && <Button disabled={disabled[0]} onClick={async () => {
                 await rej.mutateAsync({ clipId: clipId })
                 disabled[1](true)
             }} small intent="danger" icon="trash" text="reject" />
+            }
         </ButtonGroup>
     </div>
 }
