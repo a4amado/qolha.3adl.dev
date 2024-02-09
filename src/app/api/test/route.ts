eee1a742ed0251a04d7fe3370fdedfdb5f0fd905
Ahmad Addel <a4addel@gmail.com>
2024-02-09 07:30:04 +0200

    maybe

---
+++ src/app/api/test/route.ts
@@ -0,0 +1,16 @@
+import { createClient } from "@supabase/supabase-js";
+import { NextResponse } from "next/server";
+NextResponse
+export const GET = async () => {
+    const supabaseStorage = createClient(
+        'https://kvhthnqaurewrkesyryy.supabase.co',
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt2aHRobnFhdXJld3JrZXN5cnl5Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwNzQyMDkwMiwiZXhwIjoyMDIyOTk2OTAyfQ.OODYy7FaiH88wv-mxgaR4oQsnHsXrsjhh7DTFNO7lmA'
+    );
+
+    await supabaseStorage.from("word").update({
+        text: "FROM BACKEND"
+    }).eq("id", "2ec98ad0-b47b-41d3-8f75-3ba9e6ab0cad")
+    console.log("WWWWWWWWWWW");
+    return NextResponse.json({ "ss": 2 })
+
+}
