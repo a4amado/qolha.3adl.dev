a45818d73481726cc408729027365107ef65962a
a4addel <a4addel@gmail.com>
2024-03-09 00:26:43 +0200

    Update page.tsx

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -5,21 +5,30 @@ import {
   // Import predefined theme
   ThemeSupa,
 } from "@supabase/auth-ui-shared";
- import { Flex } from "antd";
+import { Flex } from "antd";
+import { useEffect } from "react";
 import { useLocation } from "react-use";
 import { supabaseclient } from "~/Auth/client";
 
 const App = () => {
   const link = useLocation();
-
+  useEffect(() => {
+    const container = document.querySelector(".ididid");
+    container?.parentElement?.parentElement?.classList.add("mx-auto")
+    container?.parentElement?.parentElement?.classList.add("w-full")
+    container?.parentElement?.parentElement?.classList.add("max-w-2xl")
+  }, [])
   return (
-   <Flex className="w-full max-w-7xl mx-auto"> <Auth
-   supabaseClient={supabaseclient}
-   appearance={{ theme: ThemeSupa }}
-   redirectTo={`${link.protocol}://${link.host}/auth`}
-   providers={[]}
- /></Flex>
-  );
+    <Auth
+    supabaseClient={supabaseclient}
+    appearance={{ theme: ThemeSupa, extend: true, className: {
+      container:  "ididid" 
+    }}}
+    redirectTo={`${link.protocol}://${link.host}/auth`}
+    providers={[]}
+    
+  />
+   );
 };
 
 export default App;
